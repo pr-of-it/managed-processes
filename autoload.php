96c5543e8c31e8fe15c2429478de0915234707bd
Степанцев Альберт <sas@weare.lt>
2018-01-31 14:59:57 +0300

    Подключили нужные библиотеки через composer

--- autoload.php
+++ autoload.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
 spl_autoload_register(function ($class) {
     if (0 === strpos($class, 'App\\')) {
         $file = __DIR__ . '/src/' . str_replace('\\', '/', substr($class, 4)) . '.php';
