f1f9ff8bda77f4b18a39e8dc0081045f7d92d209
Степанцев Альберт <sas@weare.lt>
2018-01-31 10:31:20 +0300

    Добавили определение PID воркера

--- src/Worker.php
+++ src/Worker.php
@@ -11,6 +11,22 @@ namespace App;
 abstract class Worker
 {
 
+    // Здесь хранится PID текущего процесса
+    protected $pid;
+
+    // Здесь мы узнаём PID и выводим его
+    public function __construct()
+    {
+        $this->pid = getmypid();
+        $this->message('PID=' . $this->pid);
+    }
+
+    // Вспомогательный метод, чтобы не писать везде "\n"
+    protected function message(string $message)
+    {
+        echo date('d.m.Y H:i:s') . ' ' . $message . "\n";
+    }
+
     // Метод, который и будет выполнять полезную работу
     abstract protected function handle(): \Generator;
 
@@ -19,7 +35,7 @@ abstract class Worker
     {
         foreach ($this->handle() as $done) {
             // Здесь будет находиться точка прерывания воркера
-            echo $done . '...';
+            $this->message($done . '...');
         };
         echo "\n";
     }
